bb82ece8b638c8d377706b7793217278f3b69891
MaksymYatsun <egocogitio3.14@gmail.com>
2023-10-19 01:08:56 +0300

    init

---
+++ client/src/canvas/Backdrop.jsx
@@ -0,0 +1,34 @@
+import React, { useRef } from "react";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+
+const Backdrop = () => {
+  const shadows = useRef();
+  return (
+    <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alfaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight
+        amount={5}
+        radius={9}
+        intensity={2.5}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, 5, -9]}
+      />
+    </AccumulativeShadows>
+  );
+};
+
+export default Backdrop;
